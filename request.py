a7d071896164e620a524d63ef0be478648e18be4
Guideye <99973291+Guideye@users.noreply.github.com>
2022-06-12 11:04:04 +0800

    更新功能完成
    更新功能完成

--- request.py
+++ request.py
@@ -29,11 +29,35 @@ def getText():
     page_text=requests.get(url=url1,headers=k)
     soup=BeautifulSoup(page_text.content,'lxml',from_encoding='utf-8')
     p_list=soup.select('.content>center p')
-    fp=open('./text.txt','w',encoding='utf-8')
+    fp=open('./Covid19' + time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt','w',encoding='utf-8')
     for Covid19 in p_list:
         Covid19Info=Covid19.text
         fp.write(Covid19Info+'\n')
         print(Covid19Info,'文字数据下载成功！！')
+        flag = 0
+
+    print(flag)
+    try:
+        if flag == 1:  # 成功下载，需要删除旧数据
+            with open('./Covid19 ' + time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt', 'r') as li1:
+                removptr = li1.read()
+                print(removptr)
+                os.remove(removptr)
+                print("the ./Covid19outdate file is delete")
+        else:
+            print("the ./Covid19list already deleted")
+    except:
+        print('./Covid19cannot delete')
+    try:
+        if flag == 1:
+            with open('./Covid19 ' + time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt', 'w+') as li:
+                li.write(str('./Covid19 ' + time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt'))
+                li.close
+                print("the ./Covid19 list is update")
+        else:
+            print("the ./Covid19 list already exist")
+    except:
+        print('./Covid19 cannot save')
 
 def getPolicy():
     url4 = "http://bj.bendibao.com/news/202254/314101.shtm"
@@ -44,11 +68,35 @@ def getPolicy():
     page_text=requests.get(url=url4,headers=k)
     soup=BeautifulSoup(page_text.content,'lxml',from_encoding='utf-8')
     p_list=soup.select('.content>p')
-    fp=open('./Policy.txt','w',encoding='utf-8')
+    fp=open('./Policy' +time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt','w',encoding='utf-8')
     for Covid19Policy in p_list:
         Covid19PolicyInfo=Covid19Policy.text
         fp.write(Covid19PolicyInfo+'\n')
         print(Covid19PolicyInfo,'政策数据下载成功！！')
+        flag = 0
+
+    print(flag)
+    try:
+        if flag == 1:  # 成功下载，需要删除旧数据
+            with open('./Policy' +time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt', 'r') as li1:
+                removptr = li1.read()
+                print(removptr)
+                os.remove(removptr)
+                print("thePolicy outdate file is delete")
+        else:
+            print("thePolicy list already deleted")
+    except:
+        print('Policycannot delete')
+    try:
+        if flag == 1:
+            with open('./Policy' +time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt', 'w+') as li:
+                li.write(str('./Policy' +time.strftime('%Y.%m.%d', time.localtime(time.time())) + '.txt'))
+                li.close
+                print("the Policylist is update")
+        else:
+            print("the Policylist already exist")
+    except:
+        print('Policycannot save')
 
 
 
@@ -76,10 +124,34 @@ def getIMG():
         image_data=requests.get(url=src,headers=k).content#图片二进制数据
         #生成图片名称
         image_name=src.split('/')[-1]
-        imgPath='./Area/'+image_name#储存路径
+        imgPath='./Area/' +time.strftime('%Y.%m.%d', time.localtime(time.time())) +image_name#储存路径
         with open(imgPath,'wb')as fp:
             fp.write(image_data)
             #print(image_name,'区域图片下载成功！')
+            flag = 0
+
+    print(flag)
+    try:
+        if flag == 1:  # 成功下载，需要删除旧数据
+            with open('./Area/' +time.strftime('%Y.%m.%d', time.localtime(time.time())), 'r') as li1:
+                removptr = li1.read()
+                print(removptr)
+                os.remove(removptr)
+                print("the outdate area is delete")
+        else:
+            print("the list area deleted")
+    except:
+        print('cannot delete')
+    try:
+        if flag == 1:
+            with open('./Area/' +time.strftime('%Y.%m.%d', time.localtime(time.time())), 'w+') as li:
+                li.write(str('./Area' +time.strftime('%Y.%m.%d', time.localtime(time.time()))))
+                li.close
+                print("the arealist is update")
+        else:
+            print("the arealist already exist")
+    except:
+        print('area cannot save')
 
 if __name__ == "__main__":
 
